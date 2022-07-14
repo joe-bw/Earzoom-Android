c9359ce5d0c67fb369d75ce7b5c6ad6f416e59bf
jhong <jhong@sorizava.co.kr>
2022-07-14 16:58:49 +0900

    Earzoom-2 1. 인트로 화면 개발
     [x] 이미지 추가
     [x] 폰트 추가
     [x] 인트로 화면 UI 개발 진행
    
     - repository 이관을 위한 commit
    
    Signed-off-by: jhong <jhong@sorizava.co.kr>

---
+++ app/src/main/java/com/sorizava/asrplayer/network/LoginApiClient.kt
@@ -0,0 +1,38 @@
+/*
+ * Create by jhong on 2022. 7. 13.
+ * Copyright(c) 2022. Sorizava. All rights reserved.
+ */
+
+package com.sorizava.asrplayer.network
+
+import com.sorizava.asrplayer.config.LoginManager
+import com.sorizava.asrplayer.data.ResultState
+import com.sorizava.asrplayer.data.vo.LoginDataVO
+import com.sorizava.asrplayer.data.vo.LoginNewRequest
+import com.sorizava.asrplayer.utils.NetworkConnectivity
+
+class LoginApiClient(
+    private val networkConnectivity: NetworkConnectivity,
+    private val apiService: AppApiService
+) : BaseApiClient(networkConnectivity) {
+    suspend fun requestMemberInfo(info: LoginNewRequest): ResultState<AppApiResponse<LoginDataVO>> {
+        return when (
+            val response = processCall { apiService.requestMemberInfo(info)}
+        ) {
+            is Int -> {
+                ResultState.Error(errorCode = response)
+            }
+            else -> {
+                val result = response as AppApiResponse<*>
+                if (result.status == 200) {
+                    val data: LoginDataVO = result.data as LoginDataVO
+                    val member = data.member
+                    LoginManager.instance?.prefUserId = member?.id
+                    ResultState.Success(data = response as AppApiResponse<LoginDataVO>)
+                } else {
+                    ResultState.Error(errorCode = result.status)
+                }
+            }
+        }
+    }
+}
